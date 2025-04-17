a984edf8cdc2e6c597d64723807db2fbc83657ee
edvin <edvinkeskin@gmail.com>
2025-04-16 17:03:17 -0700

    remove unused font

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,20 +1,9 @@
 'use client';
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from '@/theme/theme';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -23,9 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
           <ThemeProvider theme={theme}>
               <CssBaseline />
               {children}
